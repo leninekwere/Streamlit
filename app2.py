954ff8942891e75f21c6ef258affeed2566eb87c
leninekwere <ekwerelenin@gmail.com>
2024-07-03 09:14:49 -0700

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df=pd.read_csv("sample_pivot.csv")
+st.set_page_config(layout='wide')
+
+fh=df.drop(columns=["Region"]).columns
+st.title("HIGH FASHION :green[Analysis] 👕👔👖:")
+st.markdown("This Analysis Dashboard that gives a relationship Between **Region** and Others Varios attributes used in production of designer of **High Fashion** Wears, based on Four REgions which are (East,West,North,South)")
+
+st.sidebar.markdown('## the piechart and barchart: Relationship Between Region And Sales')
+x_axis=st.sidebar.selectbox("X AXIS",fh)
+y_axis=st.sidebar.selectbox("Y AXIS",fh)
+color_encode=st.sidebar.checkbox(label="COLOR ENCODE BY Region")
+
+container=st.container()
+chart1,chart2=container.columns(2)
+
+with chart1:
+    if x_axis and y_axis:
+        fig=px.bar(df,x=x_axis, y=y_axis, color="Region" if color_encode else None, title="{} VS {}".format(x_axis.capitalize(), y_axis.capitalize()))
+        st.plotly_chart(fig,use_container_width=True)
+with chart2:
+    if x_axis and y_axis:
+        st.markdown("SELECT X AXIS TO BE ONLY CATEGORY AND Y AXIS SHOULD BE NUMBERS")
+        fig2=px.pie(df,names=x_axis,values=y_axis,title="{}VS {}".format(x_axis.upper(), y_axis.upper), template="seaborn", hole=0.5)
+        pull_values=[0,0,0,0]
+        fig2.update_traces(pull=pull_values,textposition="inside",textinfo="percent+label")
+        st.plotly_chart(fig2,use_container_width=True)
+
+show_df=st.checkbox("SHOW DATA FRAME")
+if show_df:
+    st.dataframe(df)
+
+st.header("CHICKEN REPUBLIC🥤🍔🍗🍟")
+st.markdown("Analyzing Relationship Between Customer and Other Atrributes like(Total bill, Tips,Gender) with Each Day")
+df3=pd.read_csv("tips.csv")
+fig6=px.sunburst(df3,path=["Day","Time","Sex","Smoker"],values="Total_bill")
+st.plotly_chart(fig6,use_container_width=True)
+    
